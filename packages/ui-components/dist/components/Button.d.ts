c194704192ab2199d9a7312eff5357814d6fb5d9
ethan-heo <ethan.hb94@gmail.com>
2021-05-25 23:50:07 +0900

    remove all

--- packages/ui-components/dist/components/Button.d.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-declare type Props = {
-    className?: string;
-    children: React.ReactNode;
-};
-declare function Button({ className, children }: Props): JSX.Element;
-export default Button;
